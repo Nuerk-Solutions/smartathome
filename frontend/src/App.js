ec0f7dbcf2d861e9a5983f503005ec48dedf8ba5
Thomas Nürk <jojopaul88@gmail.com>
2021-07-23 17:10:11 +0200

    Added CarouselItem function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,32 +10,35 @@ import {ReactComponent as ChevronIcon} from './icons/chevron.svg';
 import {ReactComponent as ArrowIcon} from './icons/arrow.svg';
 import {ReactComponent as BoltIcon} from './icons/bolt.svg';
 import {DvbWidget} from "./dvb/Dvb";
+import Quote from "./qutoe/Quote";
 
 function App() {
     return (
-        <table cellPadding="0" cellSpacing="0" className="main-table">
-            <tbody>
-            <tr>
-                <td>
-                    <DvbWidget name="Malterstraße"/>
-                </td>
-                <td>
-                    <Navbar>
-                        <NavItem icon={<PlusIcon/>}/>
-                        <NavItem icon={<BellIcon/>}/>
-                        <NavItem icon={<MessengerIcon/>}/>
-                        <NavItem icon={<CaretIcon/>}>
-                            <DropdownMenu/>
-                        </NavItem>
-                    </Navbar>
-                </td>
-            </tr>
-            <tr>
-                <td>Test3</td>
-                <td>Test4</td>
-            </tr>
-            </tbody>
-        </table>
+        // <table cellPadding="0" cellSpacing="0" className="main-table">
+        //     <tbody>
+        //     <tr>
+        //         <td>
+        //             <DvbWidget name="Malterstraße"/>
+        //         </td>
+        //         <td>
+        //             <Navbar>
+        //                 <NavItem icon={<PlusIcon/>}/>
+        //                 <NavItem icon={<BellIcon/>}/>
+        //                 <NavItem icon={<MessengerIcon/>}/>
+        //                 <NavItem icon={<CaretIcon/>}>
+        //                     <DropdownMenu/>
+        //                 </NavItem>
+        //             </Navbar>
+        //         </td>
+        //     </tr>
+        //     <tr>
+        //         <td>
+                    <Quote />
+        //         </td>
+        //         <td>Test4</td>
+        //     </tr>
+        //     </tbody>
+        // </table>
     );
 }
 
--- frontend/src/qutoe/Quote.js
+++ frontend/src/qutoe/Quote.js
@@ -1,5 +1,30 @@
 import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import {Carousel} from "react-responsive-carousel";
 
-export default function () {
+export default () => (
+    <Carousel autoPlay={true} autoFocus={true} infiniteLoop={true} interval={10000} transitionTime={1000} showArrows={false} showThumbs={false} showStatus={false} swipeable={true} emulateTouch={true}>
+        <CarouselItem
+            image="https://theysaidso.com/img/qod/qod-love.jpg"
+            caption="Test1"
+        />
+        <CarouselItem
+            image="https://theysaidso.com/img/qod/qod-love.jpg"
+            caption="Test1"
+        />
+        <CarouselItem
+            image="https://theysaidso.com/img/qod/qod-love.jpg"
+            caption="Test1"
+        />
+    </Carousel>
+);
 
+function CarouselItem(props) {
+    return (
+        <div>
+            <img src={props.image} alt="Failed to load image"/>
+            {props.caption && <p className={props.className ? `legend ${props.className }` : "legend"}>{props.caption}</p>}
+        </div>
+    );
 }
+

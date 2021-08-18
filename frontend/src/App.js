27b7f9a6e18e2d33c13bee464ecfa88bf276844f
Thomas Nürk <jojopaul88@gmail.com>
2021-08-18 14:09:44 +0200

    Added Default Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,8 +14,7 @@ import Quote from "./qutoe/Quote";
 
 function App() {
     return (
-        <Router
-        forceRefresh={true}>
+        <Router>
             <Navbar>
                 <NavItem icon={<PlusIcon/>} destinationPath="/dvb"/>
                 <NavItem icon={<BellIcon/>} destinationPath="/quote"/>
@@ -29,6 +28,7 @@ function App() {
                 <Route exact path="/dvb/:stop?/:amount?/:offset?"
                        children={() => <DvbWidget name={"Malterstraße"}/>}/>
                 <Route path="/quote" children={() => <Quote />}/>
+                <DvbWidget name={"Malterstraße"}/>
             </Switch>
         </Router>
     );

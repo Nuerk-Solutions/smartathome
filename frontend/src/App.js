2f915857b3feb2760faaf389188e613fdb67efbc
Thomas Nürk <jojopaul88@gmail.com>
2021-08-28 17:33:03 +0200

    Added Cron Generator to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import {ReactComponent as PlusIcon} from './assets/icons/plus.svg';
 import {DvbWidget} from "./components/Dvb/DvbComponent";
 import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
 import Quote from "./components/Quote/QuoteComponent";
+import Guide from "./components/CronJob/guide/Guide";
 
 function App() {
     return (
@@ -12,13 +13,14 @@ function App() {
             <Navbar>
                 <NavItem icon={<PlusIcon/>} destinationPath="/dvb"/>
                 <NavItem icon={<BellIcon/>} destinationPath="/quote"/>
-                <NavItem icon={<MessengerIcon/>}/>
+                <NavItem icon={<MessengerIcon/>} destinationPath="/cron"/>
             </Navbar>
 
             <Switch>
                 <Route exact path="/dvb/:stop?/:amount?/:offset?"
                        children={() => <DvbWidget name={"Malterstraße"}/>}/>
                 <Route path="/quote" children={() => <Quote />}/>
+                <Route path="/cron" children={() => <Guide />}/>
                 <DvbWidget name={"Malterstraße"}/>
             </Switch>
         </Router>

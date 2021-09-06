1edbbb53872f5567af69a4b150af074e984c9e3a
Thomas Nürk <jojopaul88@gmail.com>
2021-09-06 11:34:42 +0200

    Updated backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const morgan = require("morgan");
+app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
-   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+app.get('/*', (req, res) => {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(2086);

622d410e2b2eed5dcf521ecdfae40c22fc25ca53
Thomas Nürk <jojopaul88@gmail.com>
2021-09-16 21:00:33 +0200

    Updated backend

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,23 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const morgan = require("morgan");
+const cors = require('cors');
+const request = require('request');
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', (req, res) => {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/pump/:state', function (req, res) {
+	request('http://nuerk.dynv6.net/cm?user=admin&password=Tasmota2021!&cmnd=Power%20' + req.params.state, function (error, response, body) {
+		console.error('error:', error); // Print the error if one occurred
+		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+		console.log('body:', body); // Print the HTML for the Google homepage.
+		res.end(body);
+	});
+})
+
+app.get('/*', function(req, res, next) {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(2086);

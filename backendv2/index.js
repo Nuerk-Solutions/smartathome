aad89b124b0e39a16af998ed9d307d90df505ece
Thomas Nürk <jojopaul88@gmail.com>
2021-09-18 20:06:21 +0200

    Added LowDB

--- backendv2/index.js
+++ backendv2/index.js
@@ -1,7 +1,16 @@
-'use strict';
 
 var path = require('path');
 var http = require('http');
+
+// Local DB requirements
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const {join} = require('path');
+
+// Local DB inits
+const adapter = new FileSync(join(__dirname,'..','db.json'));
+const db = low(adapter);
+db.defaults({ todos:[] }).write();
 
 var oas3Tools = require('oas3-tools');
 var serverPort = 8080;
@@ -15,6 +24,8 @@ var options = {
 
 var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
 var app = expressAppConfig.getApp();
+
+app.db = db;
 
 // Initialize the Swagger middleware
 http.createServer(app).listen(serverPort, function () {

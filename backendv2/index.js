a1d7851b8bb9a5c2acbd62a8dda372e7a276643e
Thomas Nürk <jojopaul88@gmail.com>
2021-09-19 13:35:20 +0200

    Alternative data validation ℹ

--- backendv2/index.js
+++ backendv2/index.js
@@ -1,92 +1,75 @@
 
-var http = require('http');
-var express = require('express');
-var app = express();
-
-var cors = require('cors');
-var bodyParser = require('body-parser');
-var swaggerTools = require('oas-tools');
-
-// Local DB requirements
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
-const {join} = require('path');
-
-// Local DB inits
-const adapter = new FileSync(join(__dirname,'..','db.json'));
-const db = low(adapter);
-db.defaults({ tasks:[] }).write();
-
+const express = require('express');
 const jsyaml = require("js-yaml");
-const fs = require("fs");
-const path = require("path");
+const path = require('path');
+const bodyParser = require('body-parser');
+const http = require('http');
+const OpenApiValidator = require('express-openapi-validator');
+var oas3Tools = require('oas3-tools');
 var serverPort = 8080;
 
-app.db = db;
+const fs = require("fs");
+const oasTools = require("oas-tools");
+const apiSpec = fs.readFileSync(path.join(__dirname, './api/openapi.yaml'), 'utf8');
+var oasDoc = jsyaml.safeLoad(apiSpec);
+var app = express();
+app.use(bodyParser.json({
+    strict: false
+}));
+// swaggerRouter configuration
+var options = {
+    routing: {
+        controllers: path.join(__dirname, './controllers')
+    },
+};
 
-// The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
-var spec = fs.readFileSync('./api/openapi.yaml', 'utf8');
-var swaggerDoc = jsyaml.safeLoad(spec);
-//
+
+
 var options_object = {
     controllers: path.join(__dirname, './controllers'),
-    checkControllers: true,
-    loglevel: 'info',
-    logfile: '/logger/log.info',
+    loglevel: 'debug',
+    // loglevel: 'none',
     strict: true,
     router: true,
     validator: true,
-    docs: {
-        apiDocs: '/api-docs',
-        apiDocsPrefix: '',
-        swaggerUi: '/docs',
-        swaggerUiPrefix: ''
-    },
-    ignoreUnknownFormats: false
+    ignoreUnknownFormats: true
 };
+oasTools.configure(options_object);
 
-swaggerTools.configure(options_object);
-
-// Initialize the Swagger middleware
-swaggerTools.initializeMiddleware(swaggerDoc, app, function (middleware) {
-    // Cross-Origin Resource Sharing
-
-    app.use(cors({
-        origin: 'http://localhost:8081',
-        methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
-        credentials: true
-    }));
-    //
-    app.use(bodyParser.json());       // to support JSON-encoded bodies
-    app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-        extended: true
-    }));
-
-    // Validate Swagger requests
-    app.use(middleware.swaggerValidator({
-        validateResponse: true
-    }));
-
-    // // Route validated requests to appropriate controller
-    app.use(middleware.swaggerRouter({
-        swaggerUi: '/openapi.json',
-        controllers: './controllers',
-        useStubs: process.env.NODE_ENV === 'development' // Conditionally turn on stubs (mock mode)
-    }));
-
-    // Serve the Swagger documents and Swagger UI
-    app.use(middleware.swaggerUi({
-        apiDocs: '/docs',
-        swaggerUi: '/swaggerUI'
-    }));
-
-    // Initialize the Swagger middleware
+oasTools.initialize(oasDoc, app, () => {
     http.createServer(app).listen(serverPort, function () {
         console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
         console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+
     });
 });
 
+
+// var expressAppConfig = oas3Tools.expressAppConfig(apiSpec, options);
+// var app = expressAppConfig.getApp();
+
+// const app = express();
+// console.log(apiSpec);
+// var cors = require('cors');
+// var swaggerTools = require('oas-tools');
+
+// Local DB requirements
+// const low = require("lowdb");
+// const FileSync = require("lowdb/adapters/FileSync");
+const {join} = require('path');
+
+// Local DB inits
+// const adapter = new FileSync(join(__dirname,'..','db.json'));
+// const db = low(adapter);
+// db.defaults({ tasks:[] }).write();
+
+// const jsyaml = require("js-yaml");
+
+// app.db = db;
+
+
+
+module.exports = app;
 
 
 

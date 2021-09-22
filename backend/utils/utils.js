e7305ecebf378d934a898da6a35da2e4d78cb3d1
Thomas Nürk <jojopaul88@gmail.com>
2021-09-22 21:16:10 +0200

    Fixed utils exports

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,6 +1,6 @@
 module.exports = require('../utils/reponseUtils.js');
 
-export function uuid(prefix) {
+module.exports = function uuid(prefix) {
     return (prefix + 'xxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx').replace(/[xy]/g, function (c) {
         const r = Math.random() * 16 | 0, v = c === 'x' ? r : r & 0x3 | 0x8;
         return v.toString(16);

3f39bef22e9f68cc8446224227ef4ef6821907f6
Thomas Nürk <jojopaul88@gmail.com>
2021-09-22 20:33:34 +0200

    Updated Utils

---
+++ backend/utils/utils.js
@@ -0,0 +1,8 @@
+module.exports = require('../utils/reponseUtils.js');
+
+export function uuid(prefix) {
+    return (prefix + 'xxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx').replace(/[xy]/g, function (c) {
+        const r = Math.random() * 16 | 0, v = c === 'x' ? r : r & 0x3 | 0x8;
+        return v.toString(16);
+    });
+}

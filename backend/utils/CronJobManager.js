3a505e297e54891e1e130f4084d529332b57acb4
Thomas Nürk <jojopaul88@gmail.com>
2021-09-25 12:48:41 +0200

    Updated CronJobManager

--- backend/utils/CronJobManager.js
+++ backend/utils/CronJobManager.js
@@ -1,5 +1,11 @@
 const CronJob = require('cron').CronJob;
+const {getDatabase, setItemValueById, getItemsFromDB, deleteItemById, deleteDatabase} = require('./DatabaseManager')
 let currentJobs = new Map();
+
+const cronAction = exports.cronAction = {
+    START: "running",
+    STOP: "stopped"
+}
 
 const createCronTask = exports.createCronTask = function (jobId, cronString) {
     const cronJob = new CronJob(cronString, function () {
@@ -9,13 +15,20 @@ const createCronTask = exports.createCronTask = function (jobId, cronString) {
     return cronJob;
 }
 
-exports.initAllCronTasks = function (db) {
-    let jobs = db.get('jobs').value();
-    jobs.forEach(job => {
-        let timer = db.get("timer").find({
+exports.initAllCronTasks = function () {
+    getItemsFromDB("jobs").forEach(job => {
+        let timer = getDatabase().get("timer").find({
             id: job.timerId
         }).value();
-        createCronTask(job.id, timer.cron).start()
+        createCronTask(job.id, timer.cron);
+        runActionOnCronTask(job.id, cronAction.START)
+    })
+}
+
+exports.finalizeAllCronTasks = function () {
+    getItemsFromDB("jobs").forEach(job => {
+        runActionOnCronTask(job.id, cronAction.STOP)
+        // deleteDatabase("jobs");
     })
 }
 
@@ -24,14 +37,15 @@ const getCronTask = exports.getCronTask = function (jobId) {
 }
 
 
-exports.runActionOnCronTask = function (jobId, action) {
+const runActionOnCronTask = exports.runActionOnCronTask = function (jobId, action) {
     switch (action) {
-        case "start":
+        case cronAction.START:
             getCronTask(jobId).start();
             break;
-        case "stop": case "abort":
+        case cronAction.STOP:
             getCronTask(jobId).stop();
             break;
     }
+    setItemValueById("jobs", jobId, 'status', action);
     return true;
 }

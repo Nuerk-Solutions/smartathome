b883755579cbfb37d22b4e5d01cf290c44c993eb
Thomas Nürk <jojopaul88@gmail.com>
2021-09-25 12:48:16 +0200

    Changed uuid exports method

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,8 +1,6 @@
-function uuid(prefix) {
+exports.uuid = function (prefix) {
     return (prefix + 'xxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx').replace(/[xy]/g, function (c) {
         const r = Math.random() * 16 | 0, v = c === 'x' ? r : r & 0x3 | 0x8;
         return v.toString(16);
     });
 }
-
-module.exports = {uuid};

ec5e7a5c28324c438769195a4aa86e2f521150a5
Thomas Nürk <jojopaul88@gmail.com>
2021-09-26 22:19:13 +0200

    Fixed css class name for LoadingSpinnerComponent

--- frontend/src/components/LoadingSpinner/LoadingSpinnerComponent.jsx
+++ frontend/src/components/LoadingSpinner/LoadingSpinnerComponent.jsx
@@ -5,7 +5,7 @@ export default (props) => {
     return (
         <div className="loadingSpinner-div">
             <svg className={props.style + " svg-spinner"} viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="50" cy="50" r="45"/>
+                <circle className={"loading-circle"} cx="50" cy="50" r="45"/>
             </svg>
             <h1 className="loadingSpinner-text">
                 <span>L</span>

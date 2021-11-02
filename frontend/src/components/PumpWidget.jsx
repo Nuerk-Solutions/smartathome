6484976e279bf8b0539d6aaec297bc7344f1dea5
Thomas Nürk <jojopaul88@gmail.com>
2021-11-02 20:09:49 +0100

    Added option to display running timer via query parameters

--- frontend/src/components/PumpWidget.jsx
+++ frontend/src/components/PumpWidget.jsx
@@ -1,21 +1,38 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {CircleCountDownOwn} from "./CircleCountDownOwn";
+import {useHistory, useLocation} from "react-router-dom";
 
 export function PumpWidget() {
 
     const [json, setJson] = useState([]);
+    const history = useHistory();
+    const search = useLocation();
 
-    useEffect (() => {
-        axios.post("http://localhost:2000/pump/timers", {duration: 55}).then(result => {
-            setJson(result.data);
-        });
+    useEffect(() => {
+        const params = new URLSearchParams(search.search)
+        if (!params.get("id")) {
+            axios.post("http://localhost:2000/pump/timers", {duration: 55}).then(result => {
+                setJson(result.data);
+                if (result.data) {
+                    params.append("id", result.data.id)
+                } else {
+                    params.delete("id")
+                }
+                history.push({search: params.toString()})
+            });
+        } else {
+            axios.get(`http://localhost:2000/pump/timers/${params.get("id")}`).then(result => {
+                setJson(result.data);
+            });
+        }
     }, []);
 
     return (
         <div>
             <CircleCountDownOwn startTime={json.startDate}
-                                endTime={json.endDate}/>
+                                endTime={json.endDate}
+                                fullTimeDuration={json.duration}/>
         </div>
     );
 }

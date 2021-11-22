a78df8e683499f09af14d3d646c2c34744fda56f
Thomas Nürk <jojopaul88@gmail.com>
2021-11-22 21:45:20 +0100

    Added optional Information and validation for the form

--- frontend/src/components/logbook/Logbook.jsx
+++ frontend/src/components/logbook/Logbook.jsx
@@ -4,23 +4,25 @@ import axois from 'axios';
 import '../weather/pump.scss'
 import moment from "moment-timezone";
 import {useHistory} from "react-router-dom";
-import useQueryParam from "../../utils/useQueryParam";
 import axios from "axios";
 import withReactContent from "sweetalert2-react-content";
 import Swal from "sweetalert2";
 import LoaderComponent from "../weather/loader/LoaderComponent";
-import './overViewTable.scss'
+import autosize from "autosize/dist/autosize";
 
 export default function () {
 
     const [isLoaded, setIsLoaded] = useState(false);
-    const [driver, setDriver] = useState(-1);
-    const [vehicle, setVehicle] = useState('1');
+    const [driver, setDriver] = useState('');
+    const [vehicle, setVehicle] = useState('2');
     const [date, setDate] = useState(moment().format('YYYY-MM-DD'));
     const [reason, setReason] = useState('Stadtfahrt');
     const [currentMileAge, setCurrentMileAge] = useState('');
     const [newMileAge, setNewMileAge] = useState('');
     const [vehicleData, setVehicleData] = useState([]);
+    const [additionalInformation, setAdditionInformation] = useState('');
+    const [fuelAmount, setFuelAmount] = useState('');
+    const [serviceDescription, setServiceDescription] = useState('');
     const MySwal = withReactContent(Swal);
     const history = useHistory();
 
@@ -82,12 +84,53 @@ export default function () {
         }
     }
 
+    // convert Additional Information Index starting by 1 to Getankt, Gewartet, default ''
+    const convertAdditionalInformation = (index) => {
+        switch (+index) {
+            case 1:
+                return 'Getankt';
+            case 2:
+                return 'Gewartet';
+            default:
+                return '';
+        }
+    }
+
+    // get value of fuel amount or serviceDescription based on additional information index
+    const getFuelAmountOrServiceDescription = (index) => {
+        switch (+index) {
+            case 1:
+                return fuelAmount;
+            case 2:
+                return serviceDescription;
+            default:
+                return '';
+        }
+    }
+
 
     // post request to http:localhost:2000/api/logbook new logbook entry as json
     // if error, display error message
     // use axois
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        // validate form input newMileAge < currentMileAge if not valid, display error message; use MySwal
+        if (!reason) {
+            const continueWithEmptyReason = await MySwal.fire({
+                title: 'Fehler',
+                text: 'Es wurde kein Grund für die Fahrt angegeben. Möchten Sie trotzdem fortfahren?',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Ja',
+                cancelButtonText: 'Nein',
+            }).then((result) => {
+                return !result.isConfirmed;
+            });
+            if (continueWithEmptyReason)
+                return;
+        }
+
         await axois.post('https://api.nuerk-solutions.de/logbook', {
             driver: convertDriver(driver),
             vehicle: {
@@ -96,7 +139,11 @@ export default function () {
                 newMileAge
             },
             date,
-            reasonForUse: reason
+            reasonForUse: reason,
+            additionalInformation: {
+                informationTyp: convertAdditionalInformation(additionalInformation),
+                information: getFuelAmountOrServiceDescription(additionalInformation)
+            }
         })
             .then(response => {
                 if (response.status === 200) {
@@ -109,20 +156,22 @@ export default function () {
                     }).then(async () => {
                         setIsLoaded(false);
                         await fetchData();
-                        setDriver(-1);
-                        setVehicle('0');
+                        setDriver('');
+                        setVehicle('2');
                         setDate(moment().format('YYYY-MM-DD'));
                         setReason('');
                         setCurrentMileAge('');
                         setNewMileAge('');
+                        setAdditionInformation('');
                     });
                 }
-                console.log(response);
             })
             .catch(error => {
                 console.log(error);
             });
     };
+
+    autosize(document.querySelectorAll('textarea'));
 
     if (!isLoaded) {
         return (
@@ -135,7 +184,7 @@ export default function () {
                     fallback={
                         <LoaderComponent loaderText={'Wettervorhersage-UI wird geladen'}/>
                     }>
-                    <div className="bg-gray-100 p-0 sm:p-12">
+                    <div className="p-0 sm:p-12">
                         <div className="mx-auto max-w-md px-6 py-12 bg-white border-0 shadow-lg sm:rounded-3xl">
                             <h1 className="text-2xl font-bold mb-8">Fahrtenbuch</h1>
                             <form id="form" onSubmit={handleSubmit}>
@@ -147,7 +196,7 @@ export default function () {
                                         onChange={(event) => setDriver(event.target.value)}
                                         className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none z-1 focus:outline-none focus:ring-0 focus:border-black border-gray-200"
                                     >
-                                        <option value="-1" disabled hidden/>
+                                        <option value="" disabled hidden/>
                                         <option value="1">Andera</option>
                                         <option value="2">Claudia</option>
                                         <option value="3">Oliver</option>
@@ -156,8 +205,6 @@ export default function () {
                                     <label htmlFor="select"
                                            className="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Fahrer
                                         auswählen</label>
-                                    <span className="text-sm text-red-600 hidden"
-                                          id="error">Option has to be selected</span>
                                 </div>
                                 <fieldset className="relative z-0 w-full p-px mb-5 border-0">
                                     <legend
@@ -165,21 +212,6 @@ export default function () {
                                         auswählen
                                     </legend>
                                     <div className="block pt-3 pb-2 space-x-4">
-                                        <label>
-                                            <input
-                                                required
-                                                checked={vehicle === '1'}
-                                                onChange={(event) => {
-                                                    setVehicle(event.target.value)
-                                                    updateVehicleData();
-                                                }}
-                                                type="radio"
-                                                name="vehicle"
-                                                value="1"
-                                                className="mr-2 text-black border-2 border-gray-300 focus:border-gray-300 focus:ring-black"
-                                            />
-                                            VW
-                                        </label>
                                         <label>
                                             <input
                                                 required
@@ -195,9 +227,22 @@ export default function () {
                                             />
                                             Ferrari
                                         </label>
+                                        <label>
+                                            <input
+                                                required
+                                                checked={vehicle === '1'}
+                                                onChange={(event) => {
+                                                    setVehicle(event.target.value)
+                                                    updateVehicleData();
+                                                }}
+                                                type="radio"
+                                                name="vehicle"
+                                                value="1"
+                                                className="mr-2 text-black border-2 border-gray-300 focus:border-gray-300 focus:ring-black"
+                                            />
+                                            VW
+                                        </label>
                                     </div>
-                                    <span className="text-sm text-red-600 hidden"
-                                          id="error">Option has to be selected</span>
                                 </fieldset>
 
                                 <div className="flex flex-row space-x-4">
@@ -237,7 +282,7 @@ export default function () {
                                         placeholder=" "
                                         className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
                                     />
-                                    <label htmlFor="name"
+                                    <label htmlFor="reasonForUse"
                                            className="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Reiseziel</label>
                                     <span className="text-sm text-red-600 hidden" id="error">Grund wird benötigt</span>
                                 </div>
@@ -253,24 +298,26 @@ export default function () {
                                         className="pt-3 pb-2 pr-12 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
                                     />
                                     <div className="absolute top-0 right-0 mt-3 mr-4 text-gray-400">km</div>
-                                    <label htmlFor="duration"
+                                    <label htmlFor="mileageBefore"
                                            className="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Aktueller
                                         Kilometerstand</label>
                                     <span className="text-sm text-red-600 hidden" id="error">Duration is required</span>
                                 </div>
 
-                                <div className="relative z-0 w-full mb-5">
+                                <div className="relative z-0 w-full mb-6">
                                     <input
+                                        min={currentMileAge}
                                         value={newMileAge}
                                         onChange={(event) => setNewMileAge(event.target.value)}
                                         required
                                         type="number"
                                         name="mileageAfter"
+                                        id="mileageAfter"
                                         placeholder=" "
-                                        className="pt-3 pb-2 pr-12 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+                                        className={`${(newMileAge && newMileAge < currentMileAge) ? 'text-red-700 border-red-200 focus:border-red-200' : ''} pt-3 pb-2 pr-12 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200`}
                                     />
                                     <div className="absolute top-0 right-0 mt-3 mr-4 text-gray-400">km</div>
-                                    <label htmlFor="duration"
+                                    <label htmlFor="mileageAfter"
                                            className="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Neuer
                                         Kilometerstand</label>
                                     <span className="text-sm text-red-600 hidden" id="error">Duration is required</span>
@@ -285,26 +332,59 @@ export default function () {
                                         Kosten: {Number((newMileAge - currentMileAge) * 0.20).toFixed(2)}€</p>
                                 </div>
                                 }
-                                {/*<div className="relative z-0 w-full mb-5">*/}
-                                {/*    <input*/}
-                                {/*        type="number"*/}
-                                {/*        name="money"*/}
-                                {/*        placeholder=" "*/}
-                                {/*        className="pt-3 pl-10 pb-2 pl-5 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"*/}
-                                {/*    />*/}
-                                {/*    <div className="absolute top-0 left-0 mt-3 ml-1 text-gray-400">👪‍</div>*/}
-                                {/*    <label htmlFor="money"*/}
-                                {/*           className="absolute duration-300 top-3 left-10 -z-1 origin-0 text-gray-500">Anzahl Passagiere</label>*/}
-                                {/*    <span className="text-sm text-red-600 hidden" id="error">Amount is required</span>*/}
-                                {/*</div>*/}
 
-                                <div className="relative z-0 w-full mb-5">
-                                    <label className="pure-material-checkbox">
-                                        <input type="checkbox"/>
-                                            <span>Beinhaltet Tankung</span>
-                                    </label>
+                                <hr className={"border-none h-1 bg-pink-500 rounded-lg"}/>
+                                <div className="relative z-0 w-full mb-5 mt-5">
+                                    <select
+
+                                        name="additionalInformation"
+                                        value={additionalInformation}
+                                        onChange={(event) => setAdditionInformation(Number(event.target.value))}
+                                        className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none z-1 focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+                                    >
+                                        <option value="" disabled hidden/>
+                                        <option value="1">Fahrzeug getankt</option>
+                                        <option value="2">Fahrzeug gewartet</option>
+                                    </select>
+                                    <label htmlFor="additionalInformation"
+                                           className="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Zusätzliche
+                                        Information</label>
+                                </div>
+
+                                {additionalInformation === 1 ?
+                                    <div className="relative z-0 w-full mb-5">
+                                        <input
+                                            min={1}
+                                            required
+                                            type="number"
+                                            name="fuelAmount"
+                                            placeholder=" "
+                                            className="pt-3 pl-10 pb-2 pl-5 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+                                            onChange={(event) => setFuelAmount(event.target.value)}
+                                        />
+                                        <div className="absolute top-0 left-0 mt-3 ml-1 text-gray-400">⛽</div>
+                                        <div className="absolute top-0 right-0 mt-3 mr-4 text-gray-400">L</div>
+                                        <label htmlFor="fuelAmount"
+                                               className="absolute duration-300 top-3 left-10 -z-1 origin-0 text-gray-500">Menge</label>
+                                        <span className="text-sm text-red-600 hidden"
+                                              id="error">Amount is required</span>
                                     </div>
+                                    : additionalInformation === 2 ?
+                                        // vehicle service with small description about 500 characters
+                                        <div className="relative z-0 w-full mb-5">
+                                            <textarea
+                                                required
+                                                id={"serviceDescription"}
+                                                placeholder=" "
+                                                className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+                                                onChange={(event) => setServiceDescription(event.target.value)}
+                                            />
+                                            <label htmlFor="serviceDescription"
+                                                   className="absolute duration-300 top-3 -z-1 origin-0 text-gray-500">Beschreibung</label>
+                                        </div>
 
+                                        : null
+                                }
                                 <button
                                     id="button"
                                     type="submit"

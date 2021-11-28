ff7a06833bddc0662b84c645de9d6f7e0228899a
Thomas Nürk <jojopaul88@gmail.com>
2021-11-28 20:59:59 +0100

    Start to impl skeleton for radio component

--- frontend/src/components/radio/ChannelItem.jsx
+++ frontend/src/components/radio/ChannelItem.jsx
@@ -1,15 +1,25 @@
 import React, {useContext} from 'react';
 import {ThemeContext} from "../../context/ThemeContext";
 
-export default function () {
+export default function (props) {
 
     const {theme, colorTheme} = useContext(ThemeContext);
 
     return (
-        <div className={`text-${colorTheme} mx-auto max-w-md px-6 py-12 border-0 shadow-lg rounded-2xl mt-5 mb-5`} style={{
+        <div className={`text-${colorTheme} w-full max-w-md max-h-96 px-6 pt-5 border-0 shadow-lg rounded-2xl mt-5 mb-5 cursor-pointer`} style={{
             backgroundColor: theme === 'dark' ? '#424242' : '#ffffff',
         }}>
-            <p>RadioChannelItem</p>
+            <div className={`radio-item`}>
+                <div className={`radio-item-container`}>
+                    <div className="radio-item-image w-40 h-40 bg-gray-200 rounded-2xl shadow-lg">
+                        <img src={props.image} />
+                    </div>
+                    <div className="radio-item-info">
+                        <div className="radio-item-name">{props.name}</div>
+                        <div className="radio-item-current-song">{"Current Song"}</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }

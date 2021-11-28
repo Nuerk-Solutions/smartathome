27ad28297ad9a9f5d23a5be836cb8804a7411635
Thomas Nürk <jojopaul88@gmail.com>
2021-11-28 17:38:11 +0100

    Styled ChannelItem

--- frontend/src/components/radio/ChannelItem.jsx
+++ frontend/src/components/radio/ChannelItem.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {ThemeContext} from "../../context/ThemeContext";
 
 export default function () {
 
+    const {theme, colorTheme} = useContext(ThemeContext);
 
     return (
-      <div className={""}>
-          <p>RadioChannelItem</p>
-      </div>
+        <div className={`text-${colorTheme} mx-auto max-w-md px-6 py-12 border-0 shadow-lg rounded-2xl mt-5 mb-5`} style={{
+            backgroundColor: theme === 'dark' ? '#424242' : '#ffffff',
+        }}>
+            <p>RadioChannelItem</p>
+        </div>
     );
 }

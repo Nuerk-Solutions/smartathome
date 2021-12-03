1fe640a22d937c5b63bf76896c425baeba5fe40d
Thomas Nürk <jojopaul88@gmail.com>
2021-12-03 19:34:34 +0100

    Fixed color sync of radioChannel

--- frontend/src/components/radio/Backdrop.jsx
+++ frontend/src/components/radio/Backdrop.jsx
@@ -1,13 +1,22 @@
 import React, {useEffect} from "react";
+import styles from './colorBackdrop.css';
 import './colorBackdrop.css';
 
 const Backdrop = ({activeColor, isPlaying}) => {
-    useEffect(() => {
-        document.documentElement.style.setProperty("--active-color", activeColor);
-    }, [activeColor]);
+
+    const gradient = {
+        background: `linear-gradient(45deg, ${activeColor || '#00aeb0'} 20%, transparent 100%) no-repeat`,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        zIndex: -1,
+    }
 
     return <div
-        className={`px-6 pt-5 border-0 shadow-lg rounded-2xl cursor-pointer color-backdrop ${isPlaying ? "playing" : "idle"}`}/>;
+        className={`px-6 pt-5 border-0 shadow-lg rounded-2xl cursor-pointer ${isPlaying ? "playing" : "idle"}`}
+        style={gradient}/>;
 };
 
 export default Backdrop;

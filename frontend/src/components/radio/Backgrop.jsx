739a27b8dab9e0a7ce84aa3cd8b06ca6d3972824
Thomas Nürk <jojopaul88@gmail.com>
2021-12-03 18:38:22 +0100

    Removed vibration from backdrop

--- frontend/src/components/radio/Backgrop.jsx
+++ frontend/src/components/radio/Backgrop.jsx
@@ -1,18 +1,13 @@
-import React, { useEffect } from "react";
-import Vibrant from "node-vibrant/lib/bundle";
+import React, {useEffect} from "react";
 import './colorBackdrop.css';
 
-const Backdrop = ({ activeColor, isPlaying }) => {
+const Backdrop = ({activeColor, isPlaying}) => {
     useEffect(() => {
-        activeColor && (
-        Vibrant.from(activeColor).getPalette().then(palette => {
-            const {vibrant, darkVibrant, lightVibrant, darkMuted, lightMuted} = palette;
-            const color = vibrant ? vibrant.hex : darkVibrant ? darkVibrant.hex : lightVibrant ? lightVibrant.hex : darkMuted ? darkMuted.hex : lightMuted ? lightMuted.hex : '#fff';
-            document.documentElement.style.setProperty("--active-color", color);
-        })) || document.documentElement.style.setProperty("--active-color", '#0048ff');
+        document.documentElement.style.setProperty("--active-color", activeColor);
     }, [activeColor]);
 
-    return <div className={`px-6 pt-5 border-0 shadow-lg rounded-2xl cursor-pointer color-backdrop ${isPlaying ? "playing" : "idle"}`} />;
+    return <div
+        className={`px-6 pt-5 border-0 shadow-lg rounded-2xl cursor-pointer color-backdrop ${isPlaying ? "playing" : "idle"}`}/>;
 };
 
 export default Backdrop;

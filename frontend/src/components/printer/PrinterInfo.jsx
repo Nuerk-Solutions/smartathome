7625b1e9663426472d8867edad704774a11ffc0c
Thomas Nürk <jojopaul88@gmail.com>
2021-12-06 21:45:26 +0100

    Added basic printer data model request in jsx

--- frontend/src/components/printer/PrinterInfo.jsx
+++ frontend/src/components/printer/PrinterInfo.jsx
@@ -9,8 +9,8 @@ export default function PrinterInfo() {
     const [isLoaded, setIsLoaded] = useState(false);
     const [error, setError] = useState(null);
 
-    useEffect(() => {
-        axios.get('http://localhost:2000/printer')
+    useEffect(async () => {
+        await axios.get('http://localhost:2000/printer')
             .then(res => {
                 setJson(res.data);
                 setIsLoaded(true);
@@ -48,11 +48,43 @@ export default function PrinterInfo() {
                     }>
                     <div>
                         {
-                            json
+                            Object.keys(json.response.PAGE_PRNINFO[0]).map((key, index) => {
+                                return (
+                                    json.response.PAGE_PRNINFO[0][key].map((item) => {
+                                        const value = item.split(',');
+                                        return (
+                                            <div key={index}> {convertInkIdToInkName(Number(value[0])) + " - " + value[1] + " - " + value[2]}</div>
+                                        );
+                                    })
+                                );
+                            })
+
                         }
                     </div>
                 </Suspense>
             </Fragment>
         );
     }
+
+    // convert InkId (Int) to InkName (String) with 0 = BK, 1 = PGBK, 2 = C, 3 = M, 4 = Y, 5 = GY
+    function convertInkIdToInkName(inkId) {
+        switch (inkId) {
+            case 0:
+                return 'BK';
+            case 1:
+                return 'PGBK';
+            case 2:
+                return 'C';
+            case 3:
+                return 'M';
+            case 4:
+                return 'Y';
+            case 5:
+                return 'GY';
+            default:
+                return 'UNKNOWN';
+        }
+    }
+
+
 }

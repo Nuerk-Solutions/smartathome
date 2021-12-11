8925395852ea895e56a823be733ae56146ec39b9
Thomas Nürk <jojopaul88@gmail.com>
2021-12-11 15:13:54 +0100

    Removed useless header

--- backend/routes/printer.js
+++ backend/routes/printer.js
@@ -12,8 +12,7 @@ router.get("/", (req, res, next) => {
         method: 'post',
         url: 'http://printer.nuerk.dynv6.net/rui/prninfo_data.cgi',
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Access-Control-Allow-Origin': '*'
+            'Content-Type': 'application/x-www-form-urlencoded'
         },
         data: params
     }).then(response => {

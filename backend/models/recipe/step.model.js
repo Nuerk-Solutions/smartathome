cd8b43cf218fdc39a35c5dc3edf9ada9cc66d154
Thomas Nürk <jojopaul88@gmail.com>
2021-12-13 17:43:38 +0100

    Added step db model

---
+++ backend/models/recipe/step.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const StepSchema = new Schema({
+    _id: Schema.Types.ObjectId,
+    recipe_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'RecipeModel'
+    },
+    step: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true});
+
+const StepModel = mongoose.model('StepModel', StepSchema, "steps");
+module.exports = StepModel;

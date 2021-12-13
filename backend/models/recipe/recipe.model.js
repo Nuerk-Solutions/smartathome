b4f73d14a88462526de21a3ccc035077edff50d8
Thomas Nürk <jojopaul88@gmail.com>
2021-12-13 17:43:45 +0100

    Added recipe db model

---
+++ backend/models/recipe/recipe.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const RecipeSchema = new Schema({
+    _id: Schema.Types.ObjectId,
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    ingredients: [{
+        type: Schema.Types.ObjectId,
+        ref: 'IngredientModel',
+        required: true
+    }],
+    steps: [{
+        type: Schema.Types.ObjectId,
+        ref: 'StepModel',
+        required: true
+    }],
+    image: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true});
+
+const RecipeModel = mongoose.model('RecipeModel', RecipeSchema, "recipes");
+module.exports = RecipeModel;

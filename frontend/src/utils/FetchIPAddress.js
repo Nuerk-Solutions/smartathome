e6d125c326ca51fbeaa105fb77e7df487acb790a
derech1e <39240736+derech1e@users.noreply.github.com>
2022-01-24 11:29:09 +0100

    Updated IP address fetching URL

--- frontend/src/utils/FetchIPAddress.js
+++ frontend/src/utils/FetchIPAddress.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const fetchIPAddress = async () => {
     try {
-        const {data} = await axios.get('http://ip-api.com/json/?lang=de')
+        const {data} = await axios.get('https://pro.ip-api.com/json?fields=66842623&key=uGZgjL0qOTI19n4&lang=de')
         return data
     } catch (err) {
         console.err(err);
